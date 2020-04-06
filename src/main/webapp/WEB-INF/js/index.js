870190421c56a6cbe12dada758f4dab25d9e3636
unknown <kutaelee0@gmail.com>
2020-04-06 14:09:10 +0900

    index 레이어 추가 및 css 적용

--- src/main/webapp/WEB-INF/js/index.js
+++ src/main/webapp/WEB-INF/js/index.js
@@ -1,11 +1,53 @@
 $(document).ready(function() {
 
-	 $('.header').load("/resources/header.html?"+new Date().getTime());
-	 var progress=document.getElementById('progressbar');
-	 var wrap=document.getElementById('wrap');
-	 var totalHeight= wrap.scrollHeight-window.innerHeight;
-	 wrap.onscroll=function(){
-		 var progressHeight=(wrap.scrollTop/totalHeight)*100;
-		 progress.style.height=progressHeight+"%";
-	 }
- });
+	$('.header').load("/resources/header.html?" + new Date().getTime());
+	var progress = document.getElementById('progressbar');
+	var wrap = document.getElementById('wrap');
+	var totalHeight = wrap.scrollHeight - window.innerHeight;
+	wrap.onscroll = function() {
+		var progressHeight = (wrap.scrollTop / totalHeight) * 100 + 3;
+		progress.style.height = progressHeight + "%";
+	}
+
+	var offset1 = $('#member-layer').offset().top;
+	var offset2 = $('#skew-layer').offset().top;
+	var offset3 = $('#cube-layer').offset().top-100;
+	var offset4 = $('#util-layer').offset().top;
+	$(document).on('click', '#scrollSpy a', function(e) {
+		var selectNum = e.target.id[3];
+
+		if (selectNum == 1) {
+			$('#wrap').animate({
+				scrollTop : offset1
+			}, 400);
+		} else if (selectNum == 2) {
+			$('#wrap').animate({
+				scrollTop : offset2
+			}, 400);
+		} else if (selectNum == 3) {
+			$('#wrap').animate({
+				scrollTop : offset3
+			}, 400);
+		} else if (selectNum == 4) {
+
+			$('#wrap').animate({
+				scrollTop : offset4
+			}, 400);
+		}
+
+	});
+	/* cube */
+	var swiper = new Swiper('.swiper-container', {
+		effect : 'cube',
+		grabCursor : true,
+		cubeEffect : {
+			shadow : true,
+			slideShadows : true,
+			shadowOffset : 20,
+			shadowScale : 0.94,
+		},
+		pagination : {
+			el : '.swiper-pagination',
+		},
+	});
+});
