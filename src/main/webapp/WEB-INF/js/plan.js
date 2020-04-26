b6a66b520409b45899b27776e90705a21be8ced3
kutaelee <kutaelee0@gmail.com>
2020-04-26 19:30:50 +0900

    header 모바일 css 적용, plan 페이지 모바일css 적용

--- src/main/webapp/WEB-INF/js/plan.js
+++ src/main/webapp/WEB-INF/js/plan.js
@@ -3,17 +3,11 @@
 		let contentCnt = 12;
         let currentMonth=now.getMonth()+1;
         let margin=20;
-    	
         $(document).ready(() => {
+        	var width=screen.width;
         	$('.header').load("/resources/header.html?" + new Date().getTime());
-            if(currentMonth!=1){       
-            	margin-=60*(currentMonth-1);
-                $('.content-wrap').css('margin-left', margin + 'vw');
-            }else{
-            	 $('.content-wrap').css('margin-left', margin+'vw');
-            }
-            
-            for (let i = 1; i <= contentCnt; i++) {
+        	console.log(width);
+        	for (let i = 1; i <= contentCnt; i++) {
                 $('.content-wrap').append('<section class="content-section" id="section' + i + '">'
                     + '<div class="content">'
                     + '<div class="content-header"><p>' + i + '월</p></div>'
@@ -23,23 +17,42 @@
             $('#section'+currentMonth+' .content-header').css('text-align','center');
         	$('#section'+(currentMonth-1)+' .content-header').css('text-align','right');
         	$('#section'+(currentMonth+1)+' .content-header').css('text-align','left');
-            $(document).on('click','.slide-btn',(e)=>{
+            
+        	var mul=60; //마진 기준값 width에 따라 다름
+        	
+        	if(width<700){
+        		mul=105;
+        		margin=-5;
+        	}
+        		if(currentMonth!=1){       
+        			margin-=mul*(currentMonth-1);
+        			$('.content-wrap').css('margin-left', margin + 'vw');
+        		}else{
+        			$('.content-wrap').css('margin-left', margin+'vw');
+        		}
+        	
+        	$(document).on('click','.slide-btn',(e)=>{
             	let targetId=e.target.id;
             	if(targetId==='slide-left-btn'){
                     if(currentMonth>1){
                         currentMonth--;
-                        margin+=60;
+                        margin+=mul;
                     }else{
                         currentMonth=12; 
-                        margin=-60*(currentMonth-1)+20;
+                        margin=-mul*(currentMonth-1)+20;
                     }
             	}else{
                     if(currentMonth<12){
                         currentMonth++;
-                        margin-=60;
+                        margin-=mul;
                     }else{
+                    	if(width<700){
+                    		margin=-5;
+                    	}else{
+                    		margin=20;
+                    	}
                         currentMonth=1;
-                        margin=20;
+                        
                     }
                     $('.current-month h1').text(currentMonth+'월');
             	}
@@ -54,10 +67,17 @@
             $('.current-month h1').text(currentMonth+'월');
             $('.current-month').hide();
         	window.onscroll = function() {
-        		if($(window).scrollTop()>100){
+        		var planTop=100;
+        		if(width<700){
+        			planTop=-10;
+        		}
+        		
+        		if($(window).scrollTop()>planTop){
         			$('.current-month').fadeIn();
         		}else{
         			$('.current-month').fadeOut();
         		}
+        	
         	}
+        	
         });
